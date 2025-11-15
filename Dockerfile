e864fa5c2bfbc2347ab3d2352b616f718d492c84
Anuj Paul <anujpaul19@outlook.com>
2025-11-15 15:13:35 -0600

    Resume Service

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+# Use JDK 21 runtime
+FROM eclipse-temurin:21-jdk-alpine
+
+#EXPOSE 8080
+
+COPY target/*.jar app.jar
+
+ENTRYPOINT ["java", "-jar", "/app.jar"]
---
+++ src/test/java/com/anuj/reseume/ReseumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anuj.reseume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReseumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
