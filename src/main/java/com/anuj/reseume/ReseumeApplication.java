cf15ad2e35448d86b2a02aa42ba04ef5d7767821
Anuj Paul <anujpaul19@outlook.com>
2025-11-15 15:13:35 -0600

    Resume Service

---
+++ src/main/java/com/anuj/reseume/ReseumeApplication.java
@@ -0,0 +1,13 @@
+package com.anuj.reseume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReseumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReseumeApplication.class, args);
+	}
+
+}
