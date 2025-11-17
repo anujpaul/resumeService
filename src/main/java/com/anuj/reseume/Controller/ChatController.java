04320c72244b5dc4c2a5f91ef2c03e24d2899b3c
Anuj Paul <anujpaul19@outlook.com>
2025-11-16 19:45:33 -0600

    Addomg post reqeuest for chat

--- src/main/java/com/anuj/reseume/Controller/ChatController.java
+++ src/main/java/com/anuj/reseume/Controller/ChatController.java
@@ -1,9 +1,20 @@
 package com.anuj.reseume.Controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.anuj.reseume.model.ChatMessage;
+
+
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
+@RequestMapping("/chat")
 public class ChatController {
 	
 	@GetMapping("/")
@@ -11,9 +22,17 @@ public class ChatController {
 		return "Welcome home!";
 	}
 	
-	@GetMapping("hello")
-	public String Hello() {
-		return "Hello World";
+	@PostMapping("hello")
+	public ResponseEntity<ChatMessage> hello(@RequestBody String message) {
+		System.out.println(message);
+		try {
+			//throw new  RuntimeException("Improper structure");
+	    	return new ResponseEntity<>(new ChatMessage(message), HttpStatus.OK);
+	    	}
+		
+		catch(Exception e) {
+			return new ResponseEntity<>(new ChatMessage("Something went wrong : " + e.getMessage()), HttpStatus.BAD_REQUEST);
+		}
 	}
 
 }
