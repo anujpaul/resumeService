c805695f1adea0aac3b9910431994189e82d2162
Anuj Paul <anujpaul19@outlook.com>
2025-11-16 19:45:33 -0600

    Addomg post reqeuest for chat

---
+++ src/main/java/com/anuj/reseume/model/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.anuj.reseume.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatMessage {
+	
+	private String message;
+	
+
+}
